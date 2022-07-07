acfe8c34d6802d2762b9e4d2ffbd4ed6b4120bf7
satyaki-das-usc <satyakid@usc.edu>
2022-07-06 17:21:33 -0700

    added instruction messages in phase 1

--- phase1.sh
+++ phase1.sh
@@ -4,3 +4,7 @@ PYTHONPATH="." python dir_creator.py
 PYTHONPATH="." python extract_src_files.py
 PYTHONPATH="." python header_files_fixer.py
 PYTHONPATH="." python ignored_file_copier.py
+echo "Phase 1 is complete. Remember to do the following:"
+echo "1. Fix the ignored files.\n"
+echo "2. Ensure ground truth file is updated."
+echo "3. Update the config file in the model directory"
